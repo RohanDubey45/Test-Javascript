b73bc09e25e47c2d7dbbbc1dc411eca106e53cb7
dubeynrohan <dubeynrohan@gmail.com>
2024-04-03 19:35:36 +0530

    Changes were added on april 3

---
+++ index.js
@@ -0,0 +1,130 @@
+// it prints on the console
+// console.log(`hello`);
+// console.log(`I like pizza`);
+// console.log("rohit sharma");
+
+//This is a comment 
+/* 
+  This 
+  is a 
+  multiline comment 
+*/
+
+// fetch the element by id and then set it's content that's it 
+// document.getElementById(`myh`).textContent = 'hello45';
+// document.getElementById(`myp`).textContent = 'this is a paragraph';
+
+
+// datatypes
+// let fullname = "Rohan Dubey";
+// let std = 15.5;
+// let age = 20;
+// let isStudent = true;
+
+// console.log(typeof fullname);
+// console.log(typeof std);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+
+// console.log(`you are a student: ${isStudent}`);
+
+// document.getElementById(`p1`).textContent = `your name is ${fullname}`;
+// document.getElementById(`p2`).textContent = `your std is ${std}`;
+// document.getElementById(`p3`).textContent = `your age is ${age} yrs old`;
+
+
+//operator precedence
+// parathethisis ()
+// exponents **
+// multiplication division modulo * / % left to right 
+// addition and subtraction + -
+
+// let students = 15;
+// students = students + 4;
+// console.log(students);
+
+// input prompt
+// window.prompt(`what is your name ?`);
+
+// let name45 = window.prompt("what is your name");
+// console.log(name45);
+
+// let username;
+
+// document.getElementById(`mybtn`).onclick = function() {
+//     username = document.getElementById(`myinp`).value;
+
+//     document.getElementById(`myh1`).textContent = `hello ${username}`;
+//     console.log(username);
+
+// }
+
+
+// let age = window.prompt(`what is your age ?`);
+
+// age = "";
+// age = Number(age);
+// // age += 1;
+// console.log(typeof age);
+
+// age = Boolean(age);
+// console.log(typeof age);
+
+// age = String(age);
+// console.log(typeof age);
+
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// document.getElementById("mybtn").onclick = function(){
+//   radius = document.getElementById("myinp").value;
+//   radius = Number(radius);
+//   circumference = 2 * PI * radius;
+
+//   document.getElementById("myh3").textContent = `The circumference is ${circumference}cm`;
+// }
+
+
+// maths functions
+// let x = -45;
+// let y = 0;
+// let z;
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(y , x);
+// z = Math.sqrt(81);
+// z = Math.log(10);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(y);
+
+// console.log(z);
+
+//rolling and getting a random number logic
+// const mybtn = document.getElementById('mybtn');
+// const mylbl = document.getElementById('mylbl');
+
+// let randomno;
+// let min = 1;
+// let max = 6;
+
+// mybtn.onclick = function(){
+//   randomno = Math.floor(Math.random() * max) + min;
+//   mylbl.textContent = randomno;
+// }
+
+
+
+
+
+
+
+
+
+
